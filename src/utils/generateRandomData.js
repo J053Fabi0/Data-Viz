1d2f8fe01e6152840da1a9147f46162ece574c1b
Amiko <senorbinario@tuta.io>
2021-12-21 19:11:11 -0700

    Corrección de error menor a la hora de generar datos.

--- src/utils/generateRandomData.js
+++ src/utils/generateRandomData.js
@@ -12,19 +12,24 @@ const generateRandomYearInterval = (numberOfYears = 22, minYear = 2000, maxYear 
  * Generar los datos al azar que alimentarán la gráfica.
  */
 const generateRandomData = () => {
+  const minIDHPosible = 0.1 * 100,
+    maxIDHPosible = 1 * 100;
   const data = {};
   // Genero el intervalo al azar de años.
   const { olderYear, earlierYear } = generateRandomYearInterval();
+
   // Itero en cada año.
   for (let year = olderYear; year <= earlierYear; year++) {
+    // Preparo el arreglo que contendrá los datos de este año.
     data[year] = [];
+
     // Itero en cada estado.
     for (let i = 0; i < 32; i++) {
       // Genero los IDH al azar.
-      const averageIDH = random(10, 99);
-      const minIDH = random(10, averageIDH - 1);
-      const maxIDH = random(averageIDH + 1, 100);
-      // Integro los datos.
+      const averageIDH = random(minIDHPosible + 1, maxIDHPosible - 1);
+      const minIDH = random(minIDHPosible, averageIDH - 1);
+      const maxIDH = random(averageIDH + 1, maxIDHPosible);
+      // Integro los datos en el arreglo.
       data[year].push({
         stateIndex: i,
         data: { minIDH: minIDH / 100, averageIDH: averageIDH / 100, maxIDH: maxIDH / 100 },
